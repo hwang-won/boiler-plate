6db92f42b85ef5b128649d78ff28afe2a17189d1
groot <99851831+hwang-won@users.noreply.github.com>
2022-05-24 16:47:46 +0900

    postman 추가

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const port = 4000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose')
+// 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// 몽고DB 연결
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://guenwon:1683111@boilerplate.m9ixi.mongodb.net/?retryWrites=true&w=majority', {
     
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log('error'))
 
-
+// test
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!')
+})
+
+// 회원가입 정보
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })  
 })
 
 app.listen(port, () => {
